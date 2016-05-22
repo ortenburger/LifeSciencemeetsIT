0e920d348461807bb894227632d9d550bcc9dded
Daniel Rexin <daniel@rx-dev.de>
2016-05-22 13:34:21 +0200

    Changed App design and functionality according to definitions in CI/medinion_app-pdf

--- medinion/www/js/services.js
+++ medinion/www/js/services.js
@@ -1,4 +1,178 @@
-angular.module('starter.services', [])
+angular.module('starter.services', ['ngResource'])
+.service('People', function($resource) {
+  return $resource('http://127.0.0.1:5000/people');
+})
+
+.service('Questionnaires', function($resource) {
+  return { "get": function() { return [{
+        "_id": "0",
+        "fields": {
+            "question": "How much do you weigh?",
+            "data": {
+                "0": {
+                    "answertype":1,
+                    "answerrange":[0,500],
+                    "answer": "weight in kg",
+                    "date": "01.01.2016",
+                    "value": 0
+                }
+            }
+        }
+    },
+{
+        "_id": "1",
+        "fields": {
+            "question": "How bad is your pain?",
+            "data": {
+                "0": {
+                    "answertype":1,
+                    "answerrange":[0,10],
+                    "answer": "No pain to near death",
+                    "date": "01.01.2016",
+                    "value": 0
+                }
+            }
+        }
+    },
+{
+        "_id": "2",
+        "fields": {
+            "question": "What is your bloodpressure?",
+            "data": {
+                "0": {
+                    "answertype":1,
+                    "answerrange":[70,200],
+                    "answer": "systolic",
+                    "date": "01.01.2016",
+                    "value": 0
+                },
+                "1": {
+                    "answertype":1,
+                    "answerrange":[40,100],
+                    "answer": "dyastolic",
+                    "date": "01.01.2016",
+                    "value": 0
+                }
+            }
+        }
+    },
+{
+        "_id": "3",
+        "fields": {
+            "question": "What is your resting pulse?",
+            "data": {
+                "0": {
+                    "answertype":1,
+                    "answerrange":[30,250],
+                    "answer": "Sit still and check your pulse or connect Fitnesstracker",
+                    "date": "01.01.2016",
+                    "value": 0
+                }
+            }
+        }
+    },
+{
+        "_id": "4",
+        "fields": {
+            "question": "Are you a Smoker?",
+            "data": {
+                "0": {
+                    "answertype":3,
+                    "answerrange":["yes"],
+                    "answer": "",
+                    "date": "01.01.2016",
+                    "value": [0]},
+
+                  "1":{
+                    "depends":"0.'value'[0]==true",
+                    "answertype":1,
+                    "answerrange":["yes"],
+                    "answer": "How many cigarettes daily?",
+                    "date": "01.01.2016",
+                    "value": 0
+                  }
+                }
+            }
+
+    },
+{
+        "_id": "5",
+        "fields": {
+            "question": "How are you feeling?",
+            "data": {
+                "0": {
+                    "answertype":1,
+                    "answerrange":[0,10],
+                    "answer": "from sad to happy",
+                    "date": "01.01.2016",
+                    "value": 0
+                }
+            }
+        }
+    },
+{
+        "_id": "6",
+        "fields": {
+            "question": "Did you poo today?",
+            "data": {
+                "0": {
+                    "answertype":3,
+                    "answerrange":["yes"],
+                    "answer": "",
+                    "date": "01.01.2016",
+                    "value": [0]
+                }
+            }
+        }
+    },
+{
+        "_id": "7",
+        "fields": {
+            "question": "Are you pregnant?",
+            "data": {
+                "0": {
+                    "answertype":3,
+                    "answerrange":["yes"],
+                    "answer": "",
+                    "date": "01.01.2016",
+                    "value": [false]
+                }
+            }
+        }
+    },
+{
+        "_id": "8",
+        "fields": {
+            "question": "How often did you pee today?",
+            "data": {
+                "0": {
+                    "answertype":1,
+                    "answerrange":[0,30],
+                    "answer": "",
+                    "date": "01.01.2016",
+                    "value": 0
+                }
+            }
+        }
+    },
+{
+        "_id": "9",
+        "fields": {
+            "question": "How many steps you took today?",
+            "data": {
+                "0": {
+                    "answertype":1,
+                    "answerrange":[0,10000],
+                    "answer": "No pain to near death",
+                    "date": "01.01.2016",
+                    "value": 0
+                }
+            }
+        }
+    }
+  ];
+
+}}})
 
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
@@ -6,7 +180,7 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
+    name: 'Ben Sparrow, MD',
     lastText: 'You on your way?',
     face: 'img/ben.png'
   }, {
@@ -16,12 +190,12 @@ angular.module('starter.services', [])
     face: 'img/max.png'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
+    name: 'Adam Bradleyson, MD',
     lastText: 'I should buy a boat',
     face: 'img/adam.jpg'
   }, {
     id: 3,
-    name: 'Perry Governor',
+    name: 'Perry Governor, MD',
     lastText: 'Look at my mukluks!',
     face: 'img/perry.png'
   }, {
